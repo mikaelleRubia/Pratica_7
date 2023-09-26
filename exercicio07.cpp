73868d97bd239821c088b5375c8b114dfc0935e4
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-26 16:26:38 -0300

    fix: ajuste em for , para o padrão 3

--- exercicio07.cpp
+++ exercicio07.cpp
@@ -26,6 +26,30 @@ int main() {
         cout << endl;
     }
 
+    cout << "Padrao 2:" << endl;
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 0; j <= altura; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    cout << "Padrao 3:" << endl;
+
+    for (int i = 1; i <= altura; i++) {
+            if (i == 1 || i == altura) {
+                for (int j = 1; j <= altura; j++) {
+                    cout << "*";
+                }
+            } else {
+                cout << "*";
+                for (int j = 2; j < altura; j++) {
+                    cout << " ";
+                }
+                cout << "*";
+            }
+            cout << endl;
+        }
 
     return 0;
 }
